d99e65d95125c28098fcb05310a680ab08ebdb9f
Edwin <edwin.syarief@gmail.com>
2016-02-28 18:38:16 +0700

    Adding Manual Transaction Model
    TODO:
    - complete MVPVM manual transaction list
    - complete MVPVM manual transaction editor

---
+++ BrawijayaWorkshopSolution/BrawijayaWorkshop.Model/ManualTransactionListModel.cs
@@ -0,0 +1,22 @@
+﻿using BrawijayaWorkshop.Database.Entities;
+using BrawijayaWorkshop.Database.Repositories;
+using BrawijayaWorkshop.Infrastructure.Repository;
+using BrawijayaWorkshop.SharedObject.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BrawijayaWorkshop.Model
+{
+    public class ManualTransactionListModel : AppBaseModel
+    {
+        private ITransactionRepository _transactionRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public ManualTransactionListModel(ITransactionRepository transactionRepository, IUnitOfWork unitOfWork)
+            :base()
+        {
+            _transactionRepository = transactionRepository;
+            _unitOfWork = unitOfWork;
+        }
+    }
+}
