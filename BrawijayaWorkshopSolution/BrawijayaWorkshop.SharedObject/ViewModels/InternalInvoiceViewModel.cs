16686a7205867609d6cf81fee0f1481faf31d58d
Edwin <edwin.syarief@gmail.com>
2016-04-06 19:57:37 +0700

    Recommit

---
+++ BrawijayaWorkshopSolution/BrawijayaWorkshop.SharedObject/ViewModels/InternalInvoiceViewModel.cs
@@ -0,0 +1,33 @@
+﻿
+namespace BrawijayaWorkshop.SharedObject.ViewModels
+{
+    public class InternalInvoiceViewModel
+    {
+        public string ReportTitle { get; set; }
+        public string CustomerName { get; set; }
+        public string VehicleNumber { get; set; }
+
+        public decimal TotalSparepart { get; set; }
+        public decimal TotalDailyMechanic { get; set; }
+        public decimal TotalContractMechanic { get; set; }
+
+        public decimal SubTotal
+        {
+            get
+            {
+                return (TotalSparepart + TotalDailyMechanic + TotalContractMechanic);
+            }
+        }
+
+        public decimal TwentyPercentCommision { get; set; }
+        public decimal TenPercenCommision { get; set; }
+
+        public decimal GrandTotal
+        {
+            get
+            {
+                return (SubTotal + TwentyPercentCommision + TenPercenCommision);
+            }
+        }
+    }
+}
