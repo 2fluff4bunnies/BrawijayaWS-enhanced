86eed1efdebf720a7a37996d06620d0d28d66f23
Edwin <edwin.syarief@gmail.com>
2016-04-06 20:59:46 +0700

    Add Internal Invoice Print Item Report

---
+++ BrawijayaWorkshopSolution/BrawijayaWorkshop.Win32App/PrintItems/InternalInvoicePrintItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BrawijayaWorkshop.Win32App.PrintItems
+{
+    public partial class InternalInvoicePrintItem : DevExpress.XtraReports.UI.XtraReport
+    {
+        public InternalInvoicePrintItem()
+        {
+            InitializeComponent();
+
+            lblDate.Text = DateTime.Today.ToString("Surabaya, dd MMMM yyyy");
+        }
+    }
+}
