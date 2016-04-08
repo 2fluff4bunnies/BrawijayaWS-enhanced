fcc712b05f2ab96e116dd3240fa5324bd8c58f67
Edwin <edwin.syarief@gmail.com>
2016-04-08 21:40:06 +0700

    Recommit

--- BrawijayaWorkshopSolution/BrawijayaWorkshop.Win32App/PrintItems/InvoicePrintItem.cs
+++ BrawijayaWorkshopSolution/BrawijayaWorkshop.Win32App/PrintItems/InvoicePrintItem.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Drawing;
-using System.Collections;
-using System.ComponentModel;
-using DevExpress.XtraReports.UI;
-using BrawijayaWorkshop.Utils;
+﻿using BrawijayaWorkshop.Utils;
+using System;
+using System.Data;
 
 namespace BrawijayaWorkshop.Win32App.PrintItems
 {
@@ -15,13 +12,12 @@ namespace BrawijayaWorkshop.Win32App.PrintItems
 
             lblInvoiceDate.Text = DateTime.Today.ToString("dd/MM/yyyy");
 
-            lblTotalInWords.BeforePrint += lblTotalInWords_BeforePrint;
+            ReportFooter.BeforePrint += ReportFooter_BeforePrint;
         }
 
-        private void lblTotalInWords_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        private void ReportFooter_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
         {
-            ((XRLabel)sender).Text = DetailReport.GetCurrentColumnValue("TotalPrice").AsDecimal().NumberToWordID();
+            lblTotalInWords.Text = ((DataRowView)GetCurrentRow()).Row["TotalPrice"].AsDecimal().NumberToWordID();
         }
-
     }
 }
