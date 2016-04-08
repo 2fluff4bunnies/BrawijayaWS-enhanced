8e7601ca22e5ef891e1b4adf4174b464b4864c55
Edwin <edwin.syarief@gmail.com>
2016-04-08 21:37:52 +0700

    Fix invoice report

--- BrawijayaWorkshopSolution/BrawijayaWorkshop.Win32App/PrintItems/InvoicePrintItem.cs
+++ BrawijayaWorkshopSolution/BrawijayaWorkshop.Win32App/PrintItems/InvoicePrintItem.cs
@@ -3,6 +3,7 @@ using System.Drawing;
 using System.Collections;
 using System.ComponentModel;
 using DevExpress.XtraReports.UI;
+using BrawijayaWorkshop.Utils;
 
 namespace BrawijayaWorkshop.Win32App.PrintItems
 {
@@ -13,6 +14,13 @@ namespace BrawijayaWorkshop.Win32App.PrintItems
             InitializeComponent();
 
             lblInvoiceDate.Text = DateTime.Today.ToString("dd/MM/yyyy");
+
+            lblTotalInWords.BeforePrint += lblTotalInWords_BeforePrint;
+        }
+
+        private void lblTotalInWords_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            ((XRLabel)sender).Text = DetailReport.GetCurrentColumnValue("TotalPrice").AsDecimal().NumberToWordID();
         }
 
     }
