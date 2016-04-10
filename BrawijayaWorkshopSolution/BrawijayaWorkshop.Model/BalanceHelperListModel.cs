2f71ef6e526f1c21f73ead06e4b5f806ebe3473d
Edwin <edwin.syarief@gmail.com>
2016-04-10 21:47:04 +0700

    Add model for balance helper list

---
+++ BrawijayaWorkshopSolution/BrawijayaWorkshop.Model/BalanceHelperListModel.cs
@@ -0,0 +1,25 @@
+﻿using BrawijayaWorkshop.Database.Entities;
+using BrawijayaWorkshop.Database.Repositories;
+using BrawijayaWorkshop.Infrastructure.Repository;
+using BrawijayaWorkshop.SharedObject.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BrawijayaWorkshop.Model
+{
+    public class BalanceHelperListModel : AppBaseModel
+    {
+        private ITransactionRepository _transactionRepository;
+        private ITransactionDetailRepository _transactionDetailRepository;
+
+        public BalanceHelperListModel(ITransactionRepository transactionRepository,
+            ITransactionDetailRepository transactionDetailRepository)
+            : base()
+        {
+            _transactionRepository = transactionRepository;
+            _transactionDetailRepository = transactionDetailRepository;
+        }
+
+
+    }
+}
