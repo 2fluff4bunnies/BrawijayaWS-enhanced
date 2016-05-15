ea5b14f6a62e69365edcec54dc8ab8702d750c1f
Edwin <edwin.syarief@gmail.com>
2016-05-15 09:14:27 +0700

    Re-commit

--- BrawijayaWorkshopSolution/BrawijayaWorkshop.Model/VehicleGroupListModel.cs
+++ BrawijayaWorkshopSolution/BrawijayaWorkshop.Model/VehicleGroupListModel.cs
@@ -1,4 +1,5 @@
-﻿using BrawijayaWorkshop.Database.Entities;
+﻿using BrawijayaWorkshop.Constant;
+using BrawijayaWorkshop.Database.Entities;
 using BrawijayaWorkshop.Database.Repositories;
 using BrawijayaWorkshop.Infrastructure.Repository;
 using BrawijayaWorkshop.SharedObject.ViewModels;
