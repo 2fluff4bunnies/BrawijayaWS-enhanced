1a1765188e5f159c66c9d18798f2fb4e9b290dd4
Edwin <edwin.syarief@gmail.com>
2016-05-16 23:12:00 +0700

    Recommit

---
+++ BrawijayaWorkshopSolution/BrawijayaWorkshop.Win32App/ModulControls/RecapInvoiceBySPKListControl.cs
@@ -0,0 +1,25 @@
+﻿using BrawijayaWorkshop.Constant;
+using BrawijayaWorkshop.Model;
+using BrawijayaWorkshop.Presenter;
+using BrawijayaWorkshop.SharedObject.ViewModels;
+using BrawijayaWorkshop.Utils;
+using BrawijayaWorkshop.View;
+using BrawijayaWorkshop.Win32App.ModulForms;
+using DevExpress.XtraGrid.Views.Grid;
+using DevExpress.XtraGrid.Views.Grid.ViewInfo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace BrawijayaWorkshop.Win32App.ModulControls
+{
+    public partial class RecapInvoiceBySPKListControl : BaseAppUserControl, IRecapInvoiceBySPKView
+    {
+        public RecapInvoiceBySPKListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
