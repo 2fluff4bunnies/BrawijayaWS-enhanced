093d01c3ff2072c7cc7bfcd47397559668cb82be
Edwin <edwin.syarief@gmail.com>
2016-05-17 14:01:53 +0700

    Update Recap

---
+++ BrawijayaWorkshopSolution/BrawijayaWorkshop.Presenter/RecapInvoiceByVehicleGroupPresenter.cs
@@ -0,0 +1,32 @@
+﻿using BrawijayaWorkshop.Infrastructure.MVP;
+using BrawijayaWorkshop.Model;
+using BrawijayaWorkshop.Runtime;
+using BrawijayaWorkshop.View;
+
+namespace BrawijayaWorkshop.Presenter
+{
+    public class RecapInvoiceByVehicleGroupPresenter : BasePresenter<IRecapInvoiceByVehicleGroupView, RecapInvoiceByVehicleGroupModel>
+    {
+        public RecapInvoiceByVehicleGroupPresenter(IRecapInvoiceByVehicleGroupView view, RecapInvoiceByVehicleGroupModel model)
+            : base(view, model) { }
+
+        public void InitFormData()
+        {
+            View.DateFrom = View.DateTo = System.DateTime.Today;
+            View.ListCategory = Model.RetrieveCategories();
+            View.ListCustomer = Model.RetrieveCustomers();
+        }
+
+        public void OnCustomerSelected()
+        {
+            View.ListVehicleGroup = Model.RetrieveVehicleGroupsByCustomerId(View.SelectedCustomer);
+            View.ListVehicle = Model.RetrieveVehiclesByCustomerId(View.SelectedCustomer);
+        }
+
+        public void LoadData()
+        {
+            View.ListInvoices = Model.RetrieveRecap(View.DateFrom, View.DateTo,
+                View.SelectedCategory, View.SelectedCustomer, View.SelectedVehicleGroup, View.SelectedVehicle);
+        }
+    }
+}
---
+++ BrawijayaWorkshopSolution/BrawijayaWorkshop.View/IRecapInvoiceByVehicleGroupView.cs
@@ -0,0 +1,27 @@
+﻿using BrawijayaWorkshop.Infrastructure.MVP;
+using BrawijayaWorkshop.SharedObject.ViewModels;
+using System;
+using System.Collections.Generic;
+
+namespace BrawijayaWorkshop.View
+{
+    public interface IRecapInvoiceByVehicleGroupView : IView
+    {
+        DateTime DateFrom { get; set; }
+        DateTime DateTo { get; set; }
+
+        int SelectedCategory { get; set; }
+        List<ReferenceViewModel> ListCategory { get; set; }
+
+        int SelectedCustomer { get; set; }
+        List<CustomerViewModel> ListCustomer { get; set; }
+
+        int SelectedVehicleGroup { get; set; }
+        List<VehicleGroupViewModel> ListVehicleGroup { get; set; }
+
+        int SelectedVehicle { get; set; }
+        List<VehicleViewModel> ListVehicle { get; set; }
+
+        List<RecapInvoiceItemViewModel> ListInvoices { get; set; }
+    }
+}
